eda1dcbb9ccbef33313cd1cc34bf59119a2c869e
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2021-12-02 10:23:42 -0300

    Receiver push notifications with FCM.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private statusBar: StatusBar,
+    private splashScreen: SplashScreen,
+    private platform: Platform,
+    private fcm: FCM
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      if (this.platform.is('ios')) {
+        this.statusBar.styleDefault();
+      } else {
+        this.statusBar.styleLightContent();
+      }
+      this.splashScreen.hide();
+
+      // subscribe to a topic
+      // this.fcm.subscribeToTopic('Notificação');
+
+      // get FCM token
+      this.fcm.getToken().then((token) => {
+        console.log(token);
+      });
+
+      // ionic push notification example
+      this.fcm.onNotification().subscribe((data) => {
+        console.log(data);
+        if (data.wasTapped) {
+          console.log('Received in background');
+        } else {
+          console.log('Received in foreground');
+        }
+      });
+
+      // refresh the FCM token
+      this.fcm.onTokenRefresh().subscribe((token) => {
+        console.log(token);
+      });
+
+      // unsubscribe from a topic
+      // this.fcm.unsubscribeFromTopic('offers');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppComponent } from './app.component';
+import { FCM } from 'cordova-plugin-fcm-with-dependecy-updated/ionic/ngx';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    FCM,
+    SplashScreen,
+    StatusBar,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
